8c1e48d385a15a0e7482744c898da8828dd5234a
yashanilaher <127527020+yashanilaher@users.noreply.github.com>
2023-06-07 21:28:23 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-msg = input("Enter the message: ")
-n = input("How many times ?: ")
-# time.sleep(3000)
-for i in range(0,int(n)):
-  pyautogui.typewrite(msg + '\n')
-  pyautogui.press("enter")
