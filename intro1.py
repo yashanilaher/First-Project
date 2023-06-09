70054b0769ea3fedd1a38c1c3b2078003a0e1e2e
yashanilaher <127527020+yashanilaher@users.noreply.github.com>
2023-06-09 08:36:15 +0530

    Update intro1.py

--- intro1.py
+++ intro1.py
@@ -11,24 +11,24 @@ root.geometry("800x800")
 
 
 def play_gif():  # will run gif with the help of tkinter
-    root.lift()  # it will bring it at top of widgets 
+    root.lift()  # it will bring it at top of widgets and remains at top of other windows
     root.attributes("-topmost", True)
     global img
-    img = Image.open("giphy.gif")
-    lb = Label(root)
+    img = Image.open("giphy.gif") # it opens gif using puthon imaging library 
+    lb = Label(root)  # it will create a widget which will display gif animation
     lb.place(x=0, y=0)  # label is place at (x=0 and y=0)
 
     # will run the loop which will blit image on screen so that gif will be visible
     mixer.music.load("bomb.mp3")  # playing music
     mixer.music.play()
 
-    for img in ImageSequence.Iterator(img):
+    for img in ImageSequence.Iterator(img): # image sequence module is used form pil to iterate each frame  of gif
         img = img.resize((1000, 800))  # so image is according to size
-        img = ImageTk.PhotoImage(img)
-        lb.config(image=img)
-        root.update()  # every time while running
-        time.sleep(0.05)  # so that it will run for few time ie 0.05 sec
-    root.destroy()
+        img = ImageTk.PhotoImage(img) # it converts the resize frame into tkinter compatible image
+        lb.config(image=img)  # will update the label widget's to current frame 
+        root.update()  # every time while running it will refresh to show updated window
+        time.sleep(0.05)  # 0.05 sec delay is made between each frame so that speed of animation is controlled
+    root.destroy()  # will destroy the window and end the programme
 
 
 play_gif()
